3cec2e6fdf35c218e5c4bc788fe67854a05c88e4
Cathbert <cmutaurwa@gmail.com>
2025-09-02 20:56:12 +0200

    Initial commit message

---
+++ src/main.py
@@ -0,0 +1,45 @@
+import flet as ft
+from flet_route import path, Routing
+from pages.loginview import LoginPage
+from pages.dashboardview import DashboardPage
+from pages.pointofsaleview import POSPage
+from pages.designerview import DesignerPage
+from pages.ordersview import OrdersPage
+from pages.clientsview import ClientPage
+from pages.toolsview import ToolPage
+import asyncio
+
+
+
+def main(page: ft.Page):
+    
+    page.theme_mode="light" # type: ignore
+    page.fonts={
+        "digital_font": "src/assets/fonts/DS-DIGIT.TTF"
+    }
+
+    define_routes = [
+        path(url="/login",view=LoginPage, clear=True),
+        path(url="/dashboard",view=DashboardPage, clear=True),
+        path(url="/pointofsale",view=POSPage, clear=True),
+        path(url="/orders",view=OrdersPage, clear=True),
+        path(url="/designer",view=DesignerPage, clear=True),
+        path(url="/clients",view=ClientPage, clear=True),
+        path(url="/tools",view=ToolPage, clear=True)
+    ]
+
+    Routing(page=page, app_routes=define_routes)
+
+    # =========================END ROUTES SECTION =============================================>
+ 
+    if page.client_storage.get("token"):
+        page.go("/clients")
+    else:
+        page.go("/login")
+
+    
+    # page.overlay.append(ft.Image(src="src/assets/MucaitLogo.png", fit=ft.ImageFit.CONTAIN, height=40, bottom=300))
+    page.update()
+
+if __name__ == "__main__":
+    ft.app(target=main, assets_dir="assets")
